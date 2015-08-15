6ad4ee9533383d7b021a360bfff031d608637e2b
Remidendooven <remi.dendooven.job@gmail.com>
2015-08-15 14:40:01 +0200

    Rajout de commentaire

--- tuto/HelloWorld.js
+++ tuto/HelloWorld.js
@@ -1,6 +1,7 @@
-
+//Un tout bête helloworld
 alert('Hello World!')
 
+//Existence de variable
 var number = 2;
 alert(typeof number); // Affiche : « number »
 
@@ -10,6 +11,7 @@ alert(typeof text); // Affiche : « string »
 var aBoolean = false;
 alert(typeof aBoolean); // Affiche : « boolean »
 
+//intéraction avec l'utilisateur
 var start = 'Bonjour ', name, end = ' !', result;
 name   = prompt('Quel est votre prénom ?');
 result = start + name + end;
@@ -21,6 +23,7 @@ second = prompt('Entrez le second chiffre :');
 result = first + second;
 alert(result);
 
+//conversion avec parseint
 var first, second, result;
 first = prompt('Entrez le premier chiffre :');
 second = prompt('Entrez le second chiffre :');
