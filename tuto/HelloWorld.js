a1c315575f50baf91478bb1a5fc8002681950dd8
Remidendooven <remi.dendooven.job@gmail.com>
2015-08-15 14:35:42 +0200

    Plusieurs petits test dont l'intéraction avec l'utilisateur

--- tuto/HelloWorld.js
+++ tuto/HelloWorld.js
@@ -9,3 +9,14 @@ alert(typeof text); // Affiche : « string »
 
 var aBoolean = false;
 alert(typeof aBoolean); // Affiche : « boolean »
+
+var start = 'Bonjour ', name, end = ' !', result;
+name   = prompt('Quel est votre prénom ?');
+result = start + name + end;
+alert(result);
+
+var first, second, result;
+first  = prompt('Entrez le premier chiffre :');
+second = prompt('Entrez le second chiffre :');
+result = first + second;
+alert(result);
