68359ccd379de88645e4cd3c53fc59c0b034f70f
Remidendooven <remi.dendooven.job@gmail.com>
2015-08-15 14:37:40 +0200

    Ajout de la conversion chaine de caractère/nombre

--- tuto/HelloWorld.js
+++ tuto/HelloWorld.js
@@ -20,3 +20,9 @@ first  = prompt('Entrez le premier chiffre :');
 second = prompt('Entrez le second chiffre :');
 result = first + second;
 alert(result);
+
+var first, second, result;
+first = prompt('Entrez le premier chiffre :');
+second = prompt('Entrez le second chiffre :');
+result = parseInt(first) + parseInt(second);
+alert(result);
